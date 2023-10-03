dd9faa2982c59902dd816322a6f5723cb25fded7
sayedanbia1 <sayedanbia5555@gamil.com>
2023-10-03 06:34:09 +0330

    frontend

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,31 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from '../../../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        this.foods = this.foodService.getAllFoodsByTag(params.tag);
+      else
+       this.foods = foodService.getAll();
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
